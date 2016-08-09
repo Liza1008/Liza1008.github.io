987cda4300f041179275a8e3c8cf8c9422de6bf5
Liza1008 <lizkyz99@gmail.com>
2016-08-09 20:21:27 +0300

    .

--- js/3-4/js/script.js
+++ js/3-4/js/script.js
@@ -1,65 +1,64 @@
 // WRAPPER - start
 var wrapper = document.createElement('div');
-	wrapper.className = 'wrapper';
-	document.body.appendChild(wrapper);
+wrapper.className = 'wrapper';
+document.body.appendChild(wrapper);
 
 var wrapper__content = document.createElement('div');
-	wrapper__content.className = 'wrapper__content';
-	wrapper.appendChild(wrapper__content);
+wrapper__content.className = 'wrapper__content';
+wrapper.appendChild(wrapper__content);
 
 // CONTENT - start
 var main_title = document.createElement('h3');
-	main_title.className = 'title';
-	main_title.innerHTML = 'Тест по программированию'; // main title
-	wrapper__content.appendChild(main_title);
+main_title.className = 'title';
+main_title.innerHTML = 'Тест по программированию'; // main title
+wrapper__content.appendChild(main_title);
 
 
 // QUESTIONS BLOCKS - start
 for (var i = 1; i <= 3; i++) {
 
-	//question 1
-	var question_sect = document.createElement('div');
-		question_sect.className = 'questionSect_checkbox';
-		wrapper__content.appendChild(question_sect);
+    //question 1
+    var question_sect = document.createElement('div');
+    question_sect.className = 'questionSect_checkbox';
+    wrapper__content.appendChild(question_sect);
 
-	var quest_title1 = document.createElement('p');
-		quest_title1.className = 'question_title';
-		quest_title1.innerHTML = i + '. Вопрос №' + i + ''; // question name
-		question_sect.appendChild(quest_title1);
+    var quest_title1 = document.createElement('p');
+    quest_title1.className = 'question_title';
+    quest_title1.innerHTML = i + '. Вопрос №' + i + ''; // question name
+    question_sect.appendChild(quest_title1);
 
-	for (var j = 1; j <= 3; j++) {
-		// answer 1
-		var checkbox1 = document.createElement('input');
-			checkbox1.className = 'checkbox';
-			checkbox1.setAttribute('type', 'checkbox');
-			checkbox1.setAttribute('id', 'checkbox' + i + '.' + j); // checkbox
+    for (var j = 1; j <= 3; j++) {
+        // answer 1
+        var checkbox1 = document.createElement('input');
+        checkbox1.className = 'checkbox';
+        checkbox1.setAttribute('type', 'checkbox');
+        checkbox1.setAttribute('id', 'checkbox' + i + '.' + j); // checkbox
 
-		var checkbox_lab1 = document.createElement('label');
-			checkbox_lab1.className = 'label'; 
+        var checkbox_lab1 = document.createElement('label');
+        checkbox_lab1.className = 'label';
 
-		var checkbox_label1 = document.createElement('span');
-			checkbox_label1.className = 'checkbox_label';
-			checkbox_label1.innerHTML = 'Вариант ответа №'+ j; // answer
+        var checkbox_label1 = document.createElement('span');
+        checkbox_label1.className = 'checkbox_label';
+        checkbox_label1.innerHTML = 'Вариант ответа №' + j; // answer
 
-		var answer1 = document.createDocumentFragment();
-			answer1.appendChild(checkbox_lab1);
-			checkbox_lab1.appendChild(checkbox1);
-			checkbox_lab1.appendChild(checkbox_label1);
-		var question_block = document.createDocumentFragment();
-			question_sect.appendChild(answer1);
-			wrapper__content.appendChild(question_block);
-	}
+        var answer1 = document.createDocumentFragment();
+        answer1.appendChild(checkbox_lab1);
+        checkbox_lab1.appendChild(checkbox1);
+        checkbox_lab1.appendChild(checkbox_label1);
+        var question_block = document.createDocumentFragment();
+        question_sect.appendChild(answer1);
+        wrapper__content.appendChild(question_block);
+    }
 }
 
 // QUESTIONS BLOCK - end
 
 //Button
 var button = document.createElement('button');
-	button.className = 'button';
-	button.innerHTML = 'Проверить мои результаты'; 
-	wrapper__content.appendChild(button); 
+button.className = 'button';
+button.innerHTML = 'Проверить мои результаты';
+wrapper__content.appendChild(button);
 
 //CONTENT - end
 
 // WRAPPER - end
-
--- js/5-6/js/script.js
+++ js/5-6/js/script.js
@@ -1,88 +1,109 @@
-var start = document.getElementById('button');
-var sec = document.getElementById('hour');
-var sec = document.getElementById('min');
-var sec = document.getElementById('sec');
-var msec = document.getElementById('msec');
+// var msec = 0;
+// var sec = 0;
+// var min = 0;
+// var hr = 0;
+// var t;
 
+// //START/STOP
+// var start_button = document.getElementById('start');
+// var stop_button = document.getElementById('stop');
 
-	min.style.fontSize = '100px';
-	sec.style.fontSize = '100px';
-	msec.style.fontSize = '100px';
-
-
-
-var countUpMsec = function() {
-	var currentTime = parseFloat(msec.textContent);
-		if (currentTime >= 0 && currentTime < 100) {
-			msec.textContent = currentTime + 1;
-		} else {
-			window.clearInterval(timer);
-		}
-} 
-var stopwatchMsec = window.setInterval(countUpMsec, 10);
-
-if (stopwatchMsec = 100) {
-	var countUpSec = function() {
-	var currentTime = parseFloat(sec.textContent);
-		if (currentTime >= 0) {
-			sec.textContent = currentTime + 1;
-		} else {
-			window.clearInterval(timer);
-		}
-} 
-
-var stopwatchSec = window.setInterval(countUpSec, 1000);
-
-var countUpMsec = function() {
-	var currentTime = parseFloat(msec.textContent);
-		if (currentTime == 100) {
-			currentTime = 0;
-			msec.textContent = currentTime + 1;
-		} else {
-			window.clearInterval(timer);
-		}
-} 
-
-var stopwatchMsec = window.setInterval(countUpMsec, 10);
-
-}
-
-if (stopwatchSec = 60) {
-	var countUpMin = function() {
-	var currentTime = parseFloat(min.textContent);
-		if (currentTime >= 0) {
-			min.textContent = currentTime + 1;
-		} else {
-			window.clearInterval(timer);
-		}
-} 
-
-var stopwatchMin = window.setInterval(countUpMin, 100000);
-
-var countUpMsec = function() {
-	var currentTime = parseFloat(sec.textContent);
-		if (currentTime == 60) {
-			currentTime = 0;
-			sec.textContent = currentTime + 1;
-		} else {
-			window.clearInterval(timer);
-		}
-} 
-
-var stopwatchSec = window.setInterval(countUpSec, 1000);
-
-}
-
-
-
-// if (stopwatch == 60) {
-// 	var countUpMin = function() {
-// 		var currentTime = parseFloat(min.textContent);
-// 			if (currentTime >= 0) {
-// 				min.textContent = currentTime + 1;
-// 			} else {
-// 				window.clearInterval(timer);
-// 			}
-// 	} 
-// var stopwatch = window.setInterval(countUpMin, 1000);
+// function stopwatch(event) {
+// 	msec++;
+// 	if (msec >= 100) {
+// 		msec = 0;
+// 		sec++;
+// 	}	
+// 	if (sec >= 60) {
+// 		sec = 0;
+// 		min++;
+// 	if(min >= 60) {
+// 		min = 0;
+// 		hr++;
+// 	}
 // }
+
+// document.getElementById("showtime").innerHTML = (hr ? (hr > 9 ? hr : "0" + hr) : "00") + ":" 
+// 												+ (min ? (min > 9 ? min : "0" + min) : "00") + ":" 	
+// 												+ (sec ? (sec > 9 ? sec : "0" + sec) : "00") + "." 
+// 												+ (msec > 9 ? msec: "0" + msec);
+// timer();
+
+// }
+
+// start_button.addEventListener('click', stopwatch);
+// if (event.which == 1) {
+// 	document.replaceChild(stop_button, start_button);
+// }
+
+// function timer() {
+// 	t = setTimeout(stopwatch, 10);
+// }
+
+// function stopTimer() {
+// 	clearTimeout(t);
+// }
+
+// //RESET
+// var reset_button = document.getElementById('reset');
+
+// function reset(event) {
+// 	msec = 0;
+// 	hr = 0;
+// 	min = 0;
+// 	sec = 0;
+// document.getElementById('showtime').innerHTML = "00:00:00.00";
+// }
+
+// reset_button.addEventListener('click', reset);
+
+// //SPLIT
+// var split_button = document.getElementById('split');
+
+// function split() {
+// 	var newtext = document.getElementById('showtime').innerHTML;
+// 	document.getElementById('text').innerHTML = document.getElementById('text').innerHTML + newtext + "\n"; 
+// }
+
+// split_button.addEventListener('click', split);
+
+// document.getElementById('text');
+// 	text.style.width = '50px';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
