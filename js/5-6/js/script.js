df85b33424bbdf582b52bea0918dbc587f683d36
Liza1008 <lizkyz99@gmail.com>
2016-08-11 16:18:28 +0300

    .

--- js/5-6/js/script.js
+++ js/5-6/js/script.js
@@ -1,9 +1,6 @@
-var status = 0; //0:stop 1:running
 var time = 0;
-var msec = 0;
-var sec = 0;
-var min = 0;
-var hr = 0;
+var status = 0; //0:stop 1:running
+
 var start_button = document.getElementById('start_button')
 var stop_button = document.getElementById('stop_button')
 var reset_button = document.getElementById('reset_button')
@@ -24,13 +21,10 @@ function start() {
     reset_button.style.display = "none";
 }
 
-start_button.addEventListener('click', start);
-
 function stop() {
     status = 0;
     start_button.style.display = 'block';
-    start_button.innerHTML = 'RESUME';
-    start_button.style.width = '150px';
+    start_button.style.width = '100px';
     stop_button.style.display = "none";
     reset_button.style.display = "block";
    	split_button.style.display = "none";
@@ -44,22 +38,21 @@ function stop() {
     stop_div.innerHTML = "<strong>STOP</strong> " + amountStops + ": " + newtext;
 }
 
-stop_button.addEventListener('click', stop);
-
 function reset() {
     status = 0;
     time = 0;
     document.getElementById('showTime').innerHTML = '00:00:00.00';
-    start_button.innerHTML = "START";
+    start_button.style.display = "block";
     start_button.style.width = '300px';
     stop_button.style.display = "none";
     reset_button.style.display = "none";
     split_button.style.display = "none";
     text.innerHTML = "";
     amountSplits = 0;
+    amountStops = 0;
 }
 
-reset_button.addEventListener('click', reset);
+
 
 function split() {
 	var split_div = document.createElement('div');
@@ -71,32 +64,39 @@ function split() {
     split_div.innerHTML = "Spit " + (amountSplits + 1) + ": " + newtext;
     text.insertBefore(split_div, allSplits[0]);
 }
-    split_button.addEventListener('click', split);
-    document.getElementById('text');
 
 function timer() {
     if (status == 1) {
-        setTimeout(function() {
-            msec++;
-            if (msec >= 100) {
-                msec = 0;
-                sec++;
-            }
+        setTimeout (function() {
+		time ++;		
+		var hours = Math.floor(time/10/60/60);;
+		var mins = Math.floor(time/100/60);
+		var seconds = Math.floor(time/95 % 60);;
+		var tenths = time % 100;
 
-            if (sec >= 60) {
-                sec = 0;
-                min++;
+		if (hours < 10) {
+		hours = '0' + hours;
+		}
 
-                if (min >= 60) {
-                    min = 0;
-                    hr++;
-                }
-            }
+		if (mins < 10) {
+			mins = '0' + mins;
+		}
+		if (seconds < 10) {
+			seconds = '0' + seconds;
+		}
 
-            document.getElementById("showTime").innerHTML = (hr ? (hr > 9 ? hr : "0" + hr) : "00") +
-                ":" + (min ? (min > 9 ? min : "0" + min) : "00") + ":" + (sec ? (sec > 9 ? sec : "0" +
-                    sec) : "00") + "." + (msec > 9 ? msec : "0" + msec);
-            timer();
-        }, 10);
+		if (tenths < 10) {
+		tenths = '0' + tenths;
+		}
+
+		document.getElementById('showTime').innerHTML = hours + ':' + mins + ':' + seconds + '.' + tenths;
+		timer();
+		}, 10);
     }
 }
+
+
+start_button.addEventListener('click', start);
+stop_button.addEventListener('click', stop);
+reset_button.addEventListener('click', reset);
+split_button.addEventListener('click', split);
